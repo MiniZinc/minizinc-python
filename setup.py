00088cfdaac1f2b6d71e8ddc2416e1ece8e28460
Jip J. Dekker <jip@dekker.one>
2018-12-28 16:25:13 +1100

    Add extra information to the setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,25 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="minizinc",
     version="0.0.1",
+    python_requires='>=3',
     author="Jip J. Dekker",
     author_email="jip.dekker@monash.edu",
     description="Access MiniZinc directly from Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="http://minizinc.org",
+    project_urls={
+        'Bug Tracker': 'https://gitlab.com/minizinc/python/issues',
+        'Documentation': 'http://minizinc.org/doc-latest',
+        'Source': 'https://gitlab.com/minizinc/python',
+    },
     packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 1 - Planning",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Mathematics",
     ],
 )
