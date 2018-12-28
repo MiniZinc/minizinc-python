c938d469154cc0716ac7f48bb8985c869381a2bc
Jip J. Dekker <jip@dekker.one>
2018-12-28 18:16:33 +1100

    Restructure functions for C API

--- minizinc/internal.py
+++ minizinc/internal.py
@@ -2,23 +2,45 @@ import re
 import shutil
 import subprocess
 
-# Oldest MiniZinc version known to work with these definitions
-REQUIRED_MINIZINC = (2, 2, 0)
+from ctypes import cdll, CDLL
 
-# Try to locate the MiniZinc executable
-__exec = shutil.which("minizinc")
+#: MiniZinc version required by the python package
+required_version = (2, 2, 0)
 
-if __exec is None:
-    raise ImportWarning("MiniZinc not found")
-else:
-    # Define internal functions using MiniZinc executable
-    def _version():
-        output = subprocess.run([__exec, "--version"], capture_output=True, check=True)
+#: Default executable used by the python package
+default_executable = None
+try:
+    # Try to load the MiniZinc C API
+    default_executable = cdll.LoadLibrary("minizinc")
+except OSError:
+    # Try to locate the MiniZinc executable
+    default_executable = shutil.which("minizinc")
+
+
+def _is_library(elem) -> bool:
+    """
+    Returns true if elem is an library format
+    """
+    return isinstance(elem, CDLL)
+
+
+def _version(minizinc=default_executable) -> tuple:
+    """
+    Returns a tuple containing the semantic version of the MiniZinc version given
+    :param minizinc: path to the minizinc executable or CDLL library
+    :return: tuple containing the minizinc version or None
+    """
+    if _is_library(minizinc):
+        raise NotImplementedError()
+    elif minizinc is not None:
+        output = subprocess.run([minizinc, "--version"], capture_output=True, check=True)
         match = re.search(r'version (\d+)\.(\d+)\.(\d+)', output.stdout.decode())
         return tuple([int(i) for i in match.groups()])
+    else:
+        raise FileNotFoundError
+
 
 # Check compatibility of the MiniZinc version
-__version = _version()
-if __version < REQUIRED_MINIZINC:
+if _version() < required_version:
     raise ImportWarning("Unsupported MiniZinc version, minimal required version %d.%d.%d (found %d.%d.%d)"
-                        % (REQUIRED_MINIZINC + __version))
+                        % (required_version + _version()))
