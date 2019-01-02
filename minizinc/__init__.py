4c8f741bf21f6a93b3bd95ddce75a93a76baf8e9
Jip J. Dekker <jip@dekker.one>
2019-01-02 15:23:21 +1100

    Add initial model classes

--- minizinc/__init__.py
+++ minizinc/__init__.py
@@ -1,4 +1,5 @@
 from .driver import load_solver, find_minizinc, default_driver
+from .model import Model, ModelInstance
 
 # Try and find MiniZinc and setup default driver
 driver.default_driver = find_minizinc("minizinc")
--- minizinc/model.py
+++ minizinc/model.py
@@ -1,2 +1,33 @@
+import os
+import tempfile
+from pathlib import Path
+
+
 class Model:
-    pass
+    def __init__(self, model: str):
+        self._mem = model
+        self._file = None
+
+    @property
+    def file(self):
+        if self._file is None:
+            self._file = tempfile.NamedTemporaryFile(prefix="minizinc_model_", suffix=".mzn")
+            self._file.write(self._mem.encode())
+            self._file.flush()
+            self._file.seek(0)
+        return self._file.name
+
+    def __del__(self):
+        if self._file is not None:
+            self._file.close()
+
+
+class ModelInstance:
+    def __init__(self, model, data=None):
+        self.files: list[Path] = []
+        if isinstance(model, Model):
+            self.files.append(Path(model.file))
+        else:
+            self.files.append(Path(os.path.abspath(model)))
+        if data is not None:
+            self.files.append(Path(os.path.abspath(data)))
