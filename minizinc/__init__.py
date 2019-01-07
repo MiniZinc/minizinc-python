5028fddce55f9a6db8a517e01256ef249bbd3763
Jip J. Dekker <jip@dekker.one>
2019-01-07 11:04:54 +1100

    Add warning when MiniZinc cannot be found

--- minizinc/__init__.py
+++ minizinc/__init__.py
@@ -3,3 +3,6 @@ from .model import Model, Instance
 
 # Try and find MiniZinc and setup default driver
 driver.default_driver = find_minizinc("minizinc")
+if driver.default_driver is None:
+    import warnings
+    warnings.warn("MiniZinc was not found on the system: no default driver could be initialised", RuntimeWarning)
