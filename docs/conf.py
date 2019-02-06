af7b2ecf6a8dcaa26cac603eb474f5a97281f162
Jip J. Dekker <jip@dekker.one>
2019-02-06 12:16:03 +1100

    Fix building source documentation when not using tox

--- docs/conf.py
+++ docs/conf.py
@@ -11,10 +11,13 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('../../src'))
+
+try:
+    import minizinc
+except ImportError:
+    import os
+    import sys
+    sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- Project information -----------------------------------------------------
