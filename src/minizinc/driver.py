ef7c86eaae06fa318b48b9db254683c2e337ae6b
Jip J. Dekker <jip@dekker.one>
2019-02-07 11:45:24 +1100

    Update documentation for the driver

--- src/minizinc/driver.py
+++ src/minizinc/driver.py
@@ -15,6 +15,12 @@ required_version = (2, 2, 0)
 
 
 class Driver(ABC):
+    """The abstract representation of a MiniZinc driver within MiniZinc Python.
+
+    Attributes:
+        Solver (Type): A specialized Solver class to be used with the Driver
+        Instance (Type): A specialized Instance class to be used with the Driver
+    """
     Solver: Type
     Instance: Type
 
@@ -30,8 +36,7 @@ class Driver(ABC):
         return ret
 
     def make_default(self) -> None:
-        """
-        Method to override the current default MiniZinc Python driver with the current object.
+        """Method to override the current default MiniZinc Python driver with the current driver.
         """
         minizinc.default_driver = self
         minizinc.Solver = self.Solver
@@ -44,10 +49,20 @@ class Driver(ABC):
 
     @abstractmethod
     def load_solver(self, tag: str):
-        """
-        Load a solver configuration from MiniZinc
-        :param tag: the id, name, or tag of the solver to load
-        :return: MiniZinc solver configuration
+        """Loads a solver configuration from the driver.
+
+        Access the MiniZinc driver's known solver configuration and load the configuation matching the given tag. Tags
+        are matched in similar to ``minizinc --solver tag``. The order of solver configuration attributes that are
+        considered is: full id, id ending, tags.
+
+        Args:
+            tag (str): tag (or id) of a solver configuration to be loaded.
+
+        Returns:
+            Solver: MiniZinc solver configuration compatible with the driver.
+
+        Raises:
+            LookupError: No configuration could be located with the given tag.
         """
         pass
 
@@ -58,35 +73,79 @@ class Driver(ABC):
               processes: Optional[int] = None,
               random_seed: Optional[int] = None,
               free_search: bool = False,
-              all_solutions=False,
+              all_solutions: bool=False,
               ignore_errors=False,
               **kwargs):
+        """Solves the Instance using the given solver configuration.
+
+        Find the solutions to the given MiniZinc instance using the given solver configuration. First, the Instance will
+        be ensured to be in a state where the solver specified in the solver configuration can understand the problem
+        and then the solver will be requested to find the appropriate solution(s) to the problem.
+
+        Args:
+            solver (Solver): The solver configuration used to compile and solve the instance
+            instance (Instance): The Instance to solve
+            timeout (Optional[timedelta]): Set the time limit for the process of solving the instance.
+            nr_solutions (Optional[int]): The requested number of solution. (Only available on satisfaction problems and
+                when the ``-n`` flag is supported by the solver).
+            processes (Optional[int]): Set the number of processes the solver can use. (Only available when the ``-p``
+                flag is supported by the solver).
+            random_seed (Optional[int]): Set the random seed for solver. (Only available when the ``-r`` flag is
+                supported by the solver).
+            free_search (bool): Allow the solver to ignore the search definition within the instance. (Only available
+                when the ``-f`` flag is supported by the solver).
+            all_solutions (bool): Request to solver to find all solutions. (Only available on satisfaction problems and
+                when the ``-n`` flag is supported by the solver)
+            ignore_errors (bool): Do not raise exceptions, when an error occurs the ``Result.status`` will be ``ERROR``.
+            **kwargs: Other flags to be passed onto the solver. (TODO: NOT YET IMPLEMENTED)
+
+        Returns:
+            Result: object containing values assigned and statistical information.
+
+        Raises:
+            MiniZincError: An error occurred while compiling or solving the model instance.
+        """
         pass
 
     @property
     @abstractmethod
     def version(self) -> str:
-        """
-        Version provides information about the version of the MiniZinc driver in use
-        :return: a string containing the version of the MiniZinc driver
+        """Reports the version of the MiniZinc Driver
+
+        Report the full version of MiniZinc as reported by the driver, including the driver name, the semantic version,
+        the build reference, and its authors.
+
+        Returns:
+            str: the version of as reported by the MiniZinc driver
         """
         pass
 
     @abstractmethod
     def check_version(self) -> bool:
-        """
-        Check if the version of the MiniZinc driver is compatible with the Minimal version required by MiniZinc Python
-        :return: result of compatibility check
+        """Check if the MiniZinc version is compatible with MiniZinc Python.
+
+        Check if the semantic version of the MiniZinc driver is compatible with the required version of the MiniZinc
+        Python driver backend.
+
+        Returns:
+            bool: The compatibility of the driver.
         """
         pass
 
 
 def find_driver(path: Optional[List[str]] = None, name: str = "minizinc") -> Optional[Driver]:
-    """
-    Find MiniZinc driver on default or specified path
-    :param path: List of locations to search
-    :param name: Name of the executable or library
-    :return: A MiniZinc Driver object, if the driver is found
+    """Finds MiniZinc Driver on default or specified path.
+
+    Find driver will look for the the MiniZinc API or the MiniZinc executable to create a Driver for MiniZinc Python. If
+    no path is specified, then the paths given by the environment variables appended by MiniZinc's default locations
+    will be tried.
+
+    Args:
+        path: List of locations to search.
+        name: Name of the API or executable.
+
+    Returns:
+        Optional[Driver]: Returns a Driver object when found or None.
     """
     if path is None:
         path_bin = os.environ.get("PATH", "").split(os.pathsep)
