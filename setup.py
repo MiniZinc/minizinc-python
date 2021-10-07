7cf287f366287a9d960f93181696ca84d0f655d0
Jip J. Dekker <jip@dekker.one>
2021-10-07 11:37:30 +1100

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
