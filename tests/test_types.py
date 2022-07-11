11d8199ca4c343c5cffaf854656f26b6765babf2
Jip J. Dekker <jip@dekker.one>
2022-07-11 11:54:39 +1000

    Enable annotation output tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -222,17 +222,15 @@ class TestNumPy(InstanceTestCase):
         assert result.status is Status.SATISFIED
 
 
-# Enable on MiniZinc 2.6+ (JSON output of annotations is broken before then)
-# class TestAnn(InstanceTestCase):
-#     def test_ann_atom(self):
-#         self.instance.add_string("ann: x :: add_to_output = promise_total;")
-#         result = self.instance.solve()
-#         assert result.status is Status.SATISFIED
-#         assert result["x"] == "promise_total"
+class TestAnn(InstanceTestCase):
+    def test_ann_atom(self):
+        self.instance.add_string("ann: x :: add_to_output = promise_total;")
+        result = self.instance.solve()
+        assert result.status is Status.SATISFIED
+        assert result["x"] == "promise_total"
 
-#     def test_ann_call(self):
-#         self.instance.add_string(
-#         'ann: x :: add_to_output = expression_name("test");')
-#         result = self.instance.solve()
-#         assert result.status is Status.SATISFIED
-#         assert result["x"] == 'expression_name("test")'
+    def test_ann_call(self):
+        self.instance.add_string('ann: x :: add_to_output = expression_name("test");')
+        result = self.instance.solve()
+        assert result.status is Status.SATISFIED
+        assert result["x"] == 'expression_name("test")'
