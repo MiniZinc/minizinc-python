fc8a422979b1960fc3da75deb772e5ce7c965bd0
Jip J. Dekker <jip@dekker.one>
2023-08-15 10:07:10 +1000

    Add a more programmatic interface for the mzn-analyse tool

--- src/minizinc/analyse.py
+++ src/minizinc/analyse.py
@@ -1,15 +1,23 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import json
 import os
 import platform
 import shutil
 import subprocess
+from enum import Enum, auto
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 from .driver import MAC_LOCATIONS, WIN_LOCATIONS
 from .error import ConfigurationError, MiniZincError
+
+
+class InlineOption(Enum):
+    DISABLED = auto()
+    NON_LIBRARY = auto()
+    ALL = auto()
 
 
 class MznAnalyse:
@@ -64,13 +72,40 @@ class MznAnalyse:
     def run(
         self,
         mzn_files: List[Path],
-        args: List[str],
-    ) -> None:
+        inline_includes: InlineOption = InlineOption.DISABLED,
+        remove_litter: bool = False,
+        get_diversity_anns: bool = False,
+        get_solve_anns: bool = True,
+        output_all: bool = True,
+        mzn_output: Optional[Path] = None,
+        remove_anns: Optional[List[str]] = None,
+        remove_items: Optional[List[str]] = None,
+    ) -> Dict[str, Any]:
         # Do not change the order of the arguments 'inline-includes', 'remove-items:output', 'remove-litter' and 'get-diversity-anns'
-        tool_run_cmd: List[Union[str, Path]] = [self._executable]
+        tool_run_cmd: List[Union[str, Path]] = [str(self._executable), "json_out:-"]
 
-        tool_run_cmd.extend(mzn_files)
-        tool_run_cmd.extend(args)
+        for f in mzn_files:
+            tool_run_cmd.append(str(f))
+
+        if inline_includes == InlineOption.ALL:
+            tool_run_cmd.append("inline-all_includes")
+        elif inline_includes == InlineOption.NON_LIBRARY:
+            tool_run_cmd.append("inline-includes")
+
+        if remove_items is not None and len(remove_items) > 0:
+            tool_run_cmd.append(f"remove-items:{','.join(remove_items)}")
+        if remove_anns is not None and len(remove_anns) > 0:
+            tool_run_cmd.append(f"remove-anns:{','.join(remove_anns)}")
+
+        if remove_litter:
+            tool_run_cmd.append("remove-litter")
+        if get_diversity_anns:
+            tool_run_cmd.append("get-diversity-anns")
+
+        if mzn_output is not None:
+            tool_run_cmd.append(f"out:{str(mzn_output)}")
+        else:
+            tool_run_cmd.append("no_out")
 
         # Extract the diversity annotations.
         proc = subprocess.run(
@@ -78,3 +113,4 @@ class MznAnalyse:
         )
         if proc.returncode != 0:
             raise MiniZincError(message=str(proc.stderr))
+        return json.loads(proc.stdout)
